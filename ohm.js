85bfd6d09eee826fc262cc738477a8aa501fdd71
sebpoirier <sebpoirier@users.noreply.github.com>
2019-11-03 20:24:37 +0100

    Simplified javascript.

--- ohm.js
+++ ohm.js
@@ -1,38 +1,11 @@
 function calculate() {
 	// Get the values of the input fields
-	var V = document.getElementById("V").value * document.getElementById("prefix_V").value;
-	var R = document.getElementById("R").value * document.getElementById("prefix_R").value;
-	var I = document.getElementById("I").value * document.getElementById("prefix_I").value;
+	var V = document.getElementById("V").value * document.getElementById("V_prefix").value;
+	var R = document.getElementById("R").value * document.getElementById("R_prefix").value;
+	var I = document.getElementById("I").value * document.getElementById("I_prefix").value;
 
-	// Calculate
-	if (document.getElementById("V").disabled) {
-		document.getElementById("V").value = R * I;
-		document.getElementById("V").value /= document.getElementById("prefix_V").value;
-	}
-	if (document.getElementById("R").disabled) {
-		document.getElementById("R").value = V / I;
-		document.getElementById("R").value /= document.getElementById("prefix_R").value;
-	}
-	if (document.getElementById("I").disabled) {
-		document.getElementById("I").value = V / R;
-		document.getElementById("I").value /= document.getElementById("prefix_I").value;
-	}
-}
-
-function button_V() {
-	document.getElementById("V").disabled = true;
-	document.getElementById("R").disabled = false;
-	document.getElementById("I").disabled = false;
-}
-
-function button_R() {
-	document.getElementById("V").disabled = false;
-	document.getElementById("R").disabled = true;
-	document.getElementById("I").disabled = false;
-}
-
-function button_I() {
-	document.getElementById("V").disabled = false;
-	document.getElementById("R").disabled = false;
-	document.getElementById("I").disabled = true;
+	// Calculate	
+	assign_if_matches("V", R * I);
+	assign_if_matches("R", V / I);
+	assign_if_matches("I", V / R);
 }
--- utils.js
+++ utils.js
@@ -5,6 +5,22 @@ function prefix_name(num) {
 	else if (num >= 1e-6) return "u";
 }
 
+function assign_if_matches(name, value) {
+	if (document.getElementById(name).disabled) {
+		document.getElementById(name).value = value;
+		document.getElementById(name).value /= document.getElementById(name + "_prefix").value;
+	}
+}
+
+function button_disable(name) {
+	var cusid_ele = document.getElementsByClassName("input_number");
+	for (var i = 0; i < cusid_ele.length; i++) {
+		cusid_ele[i].disabled=false;  
+	}
+	document.getElementById(name).disabled = true;
+}
+
+
 /*
 function remember() {
 	// Append to paragraph
