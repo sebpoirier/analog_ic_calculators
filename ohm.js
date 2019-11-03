4607090ca5efa3aad9264d534737ba899f0a44fa
sebpoirier <sebpoirier@users.noreply.github.com>
2019-11-03 14:49:18 +0100

    Added css, simplified UI.

--- ohm.js
+++ ohm.js
@@ -5,17 +5,35 @@ function calculate() {
 	var I = document.getElementById("I").value * document.getElementById("prefix_I").value;
 
 	// Calculate
-	var sel = document.getElementById("param").value;
-	switch(sel) {
-		case "V":
-			var param = R * I;
-			break;
-		case "R":
-			var param = V / I;
-			break;
-		case "I":
-			var param = V / R;
-			break;
+	if (document.getElementById("V").disabled) {
+		var sel = "V";
+		var param = R * I;
+	}
+	else if (document.getElementById("R").disabled)  {
+		var sel = "R";
+		var param = V / I;
+	}
+	else if (document.getElementById("I").disabled) {
+		var sel = "I";
+		var param = V / R;
 	}
 	document.getElementById(sel).value = param/document.getElementById("prefix_" + sel).value;
+}
+
+function button_V() {
+	document.getElementById("V").disabled = true;
+	document.getElementById("R").disabled = false;
+	document.getElementById("I").disabled = false;
+}
+
+function button_R() {
+	document.getElementById("V").disabled = false;
+	document.getElementById("R").disabled = true;
+	document.getElementById("I").disabled = false;
+}
+
+function button_I() {
+	document.getElementById("V").disabled = false;
+	document.getElementById("R").disabled = false;
+	document.getElementById("I").disabled = true;
 }
