00433ec396dc94b038aa14af14875c08b52955b3
sebpoirier <sebpoirier@users.noreply.github.com>
2019-11-16 06:36:25 +0100

    Added adc resolution.

---
+++ adc_resolution.js
@@ -0,0 +1,22 @@
+var info = {
+    "inout": {
+        "ref": {"unit": ""},
+        "n_bit": {"unit": ""},
+        "LSB": {"unit": ""}
+    }
+};
+
+function lsb_calc(ref, n_bit) {
+    var LSB = ref / 2**n_bit;
+    return ([LSB]);
+}
+
+function ref_calc(LSB, n_bit) {
+    var ref = LSB * 2**n_bit;
+    return ([ref]);
+}
+
+function n_bit_calc(ref, LSB) {
+    var n_bit = Math.log2(ref / LSB);
+    return ([n_bit]);
+}
