deb0ebb12585fa89396dd9d8b9b1fbe5bc1c029f
sebpoirier <57289852+sebpoirier@users.noreply.github.com>
2021-07-14 18:34:12 +0200

    Added additional outputs to rlc.

--- rlc.js
+++ rlc.js
@@ -4,12 +4,16 @@ var info = {
         "L": {"unit": "H"},
         "C": {"unit": "C"},
         "fc": {"unit": "Hz"},
-        "Q": {"unit": ""}
+        "Q": {"unit": ""},
+        "Tau": {"unit": "s"},
+        "damp_factor": {"unit": ""}
     }
 };
 
 function fc_calc(R, L, C) {
     var fc = 1 / (6.2832 * Math.sqrt(L * C));
     var Q = 1 / R * Math.sqrt(L / C);
-    return ([fc, Q]);
+    var Tau = 2 * L / R;
+    var damp_factor = R/2 * Math.sqrt(C / L);
+    return ([fc, Q, Tau, damp_factor]);
 }
